c89155e8694ae40b296130127a02ac6071d6a883
Lethargy <Letharqy@gmail.com>
2014-12-26 23:46:54 +1100

    ふかつどう: Fix Heads Up hiding
    My mistake, stock behavior is to dismiss in all three directions.
    
    Fixed.
    
    Swipe up = hide
    Swipe down = hide then open notification drawer so it appears there instead
    Swipe left/right = dismiss

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
@@ -479,11 +479,11 @@ public class HeadsUpNotificationView extends LinearLayout implements SwipeHelper
                             // can see it now in the notification panel.
                             if (DEBUG_EDGE_SWIPE) Log.d(TAG, "found an open");
                             mBar.animateExpandNotificationsPanel();
-                            mBar.onHeadsUpDismissed(true);
+                            mBar.scheduleHeadsUpClose();
 			}
                         if (dY < 0) {
                             if (DEBUG_EDGE_SWIPE) Log.d(TAG, "found a close");
-                            mBar.onHeadsUpDismissed(true);
+                            mBar.scheduleHeadsUpClose();
                         }
                         mConsuming = true;
                     }
