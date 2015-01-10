4fce2c398746bf1160aff32c30e8a9b3beebf601
Lethargy <Letharqy@gmail.com>
2015-01-11 05:55:10 +1100

    Revert "Fix - place Clear Recents button above the nav bar"
    This reverts commit 0dcc7cc38ef02ccb6f30b54d58a92f3a666d85a7.

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -33,7 +33,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowInsets;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import com.android.systemui.recents.Constants;
 import com.android.systemui.recents.RecentsConfiguration;
 import com.android.systemui.recents.misc.Console;
